d4675d51df6f35a0b11cbc2f3cebb49a86578511
Michael Okafor <Mike.okafor17@gmail.com>
2025-06-06 18:35:54 -0400

    feat: add collection cache files and subspecialty mapping

---
+++ scripts/subspecialty_map.py
@@ -0,0 +1,76 @@
+# Subspecialty mapping
+subspecialty_map = {
+    "neuroradiology": [
+        "TCGA-GBM", "UPENN-GBM", "ICDC-Glioma", "Vestibular-Schwannoma-SEG",
+        "Vestibular-Schwannoma-MC-RC", "MIDRC-RICORD-1A", "MIDRC-RICORD-1B", "MIDRC-RICORD-1C",
+        "RIDER PHANTOM MRI", "RIDER Pilot", "CMMD", "GBM-DSC-MRI-DRO", "ISPY2",
+        "NBIA Pediatric Brain", "Childhood Brain Tumor Network", "NBIATestData",
+        "MDACC-NIH-CDF-FAIRCHANCE"
+    ],
+    "breast": [
+        "TCGA-BRCA", "CBIS-DDSM", "Breast-Diagnosis", "Breast-MRI-NACT-Pilot",
+        "ACRIN-6698", "ACRIN-Contralateral-Breast-MR", "Advanced-MRI-Breast-Lesions",
+        "Duke-Breast-Cancer-MRI", "Breast-Cancer-Screening-DBT", "QIN-BREAST", 
+        "QIN Breast DCE-MRI", "ISPY1", "RIDER Breast MRI", "ACRIN-FLT-Breast",
+        "CMB-BRCA"
+    ],
+    "msk": [
+        "TCGA-SARC", "Soft-tissue-Sarcoma", "CPTAC-SAR", "Spine-Mets-CT-SEG", "CPTAC-CCRCC",
+        "Pelvic-Reference-Data", "Prostate-Anatomical-Edge-Cases"
+    ],
+    "cardiothoracic": [
+        "LIDC-IDRI", "RIDER Lung CT", "RIDER Lung PET-CT", "QIN LUNG CT",
+        "LungCT-Diagnosis", "Lung-PET-CT-Dx", "Anti-PD-1_Lung", "NSCLC Radiogenomics",
+        "NSCLC-Radiomics", "NSCLC-Radiomics-Genomics", "NSCLC-Radiomics-Interobserver1",
+        "APOLLO-5-LUAD", "APOLLO-5-THYM", "APOLLO-5-LUNG-MISC", "TCGA-LUAD", "TCGA-LUSC",
+        "CPTAC-LSCC", "CPTAC-LUAD", "APOLLO-5-LSCC", "Lung Phantom", "Lung-Fused-CT-Pathology",
+        "SPIE-AAPM Lung CT Challenge", "COVID-19-AR", "COVID-19-NY-SBU", "covid-19",
+        "CT4Harmonization-Multicentric"
+    ],
+    "abdominal": [
+        "TCGA-COAD", "TCGA-LIHC", "TCGA-KIRC", "TCGA-KIRP", "TCGA-KICH", "TCGA-PAAD", "Pancreas-CT",
+        "Pancreatic-CT-CBCT-SEG", "CT COLONOGRAPHY", "CT Lymph Nodes", "StageII-Colorectal-CT",
+        "Colorectal-Liver-Metastases", "CTpred-Sunitinib-panNET", "CPTAC-LIHC", "CPTAC-CHOL",
+        "PDMR-Texture-Analysis", "HCC-TACE-Seg", "C4KC-KiTS", "Adrenal-ACC-Ki67-Seg",
+        "TCGA-ESCA", "APOLLO-5-ESCA", "APOLLO-5-PAAD", "CPTAC-PDA", "QIBA-CT-Liver-Phantom",
+        "B-mode-and-CEUS-Liver", "CC-Tumor-Heterogeneity", "Mediastinal-Lymph-Node-SEG",
+        "LCTSC"
+    ],
+    "nuclear": [
+        "ACRIN-NSCLC-FDG-PET", "FDG-PET-CT-Lesions", "QIN PET Phantom", "RIDER PHANTOM PET-CT",
+        "NaF PROSTATE", "CT-vs-PET-Ventilation-Imaging", "QIBA CT-1C"
+    ],
+    "pediatric": [
+        "Pediatric-CT-SEG", "NBIA Pediatric Brain", "Childhood Brain Tumor Network", "CPTAC-Pediatric"
+    ],
+    "other": [
+        # Phantom and calibration collections
+        "CC-Radiomics-Phantom", "CC-Radiomics-Phantom-2", "CC-Radiomics-Phantom-3",
+        "Phantom FDA", "CT-Phantom4Radiomics",
+        
+        # Prostate collections
+        "PROSTATE-DIAGNOSIS", "PROSTATE-MRI", "PROSTATEx", "Prostate Fused-MRI-Pathology",
+        "Prostate-3T", "Prostate-MRI-US-Biopsy", "QIN-PROSTATE-Repeatability",
+        "CMB-PCA",
+        
+        # Other TCGA collections
+        "TCGA-BLCA", "TCGA-CESC", "TCGA-OV", "TCGA-PRAD", "TCGA-READ", "TCGA-STAD", "TCGA-THCA",
+        "TCGA-UCEC",
+        
+        # Other CPTAC collections
+        "CPTAC-UCEC", "CPTAC-CM", "CPTAC-CESC", "CPTAC-STAD", "CPTAC-AML",
+        
+        # CMB collections
+        "CMB-MEL", "CMB-MML", "CMB-LCA", "CMB-CRC", "CMB-GEC", "CMB-AML", "CMB-OV",
+        
+        # PDMR collections
+        "PDMR-292921-168-R", "PDMR-BL0293-F563", "PDMR-425362-245-T", "PDMR-997537-175-T",
+        "PDMR-833975-119-R", "PDMR-521955-158-R4",
+        
+        # Other collections
+        "EA1141", "ReMIND", "VAREPOP-APOLLO", "MIDI-B-Synthetic-Test",
+        "MIDI-B-Synthetic-Validation", "MIDI-B-Curated-Test", "MIDI-B-Curated-Validation",
+        "Training-Pseudo", "Pseudo-PHI-DICOM-Data", "DRO-Toolkit", "Mouse-Astrocytoma",
+        "Mouse-Mammary", "MRI-DIR", "VICTRE", "4D-Lung", "APOLLO"
+    ]
+} 
